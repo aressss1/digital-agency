7efe445a604f5350d21549525236325ff324a195
aressss1 <codewithfaizi@gmail.com>
2023-12-27 11:43:31 +0500

    nav-

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+    return ( 
+        <div className=" bg-stone-950 text-white flex justify-between lg:px-20 py-10" >
+            <Image 
+                src="logo.svg"
+                alt="logo"
+            />
+            <div className="flex gap-4 items-center " >
+                <div className="text-white text-base font-normal font-['Clash Display']">About us</div>
+                <div className="text-white text-base font-normal font-['Clash Display']">Store</div>
+                <div className="text-white text-base font-normal font-['Clash Display']">Games</div>
+                <Button className="text-base font-semibold font-['Clash Display']" >
+                    Connect Wallet
+                </Button>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
