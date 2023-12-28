781f49745634212cf3552b3a842e47800bdcd127
aressss1 <codewithfaizi@gmail.com>
2023-12-28 11:14:54 +0500

    f-

---
+++ components/hero-section.tsx
@@ -0,0 +1,16 @@
+const HeroSection = () => {
+    return ( 
+        <div className="flex flex-col lg:px-20 md:px-1 px-4 py-5 bg-stone-50 " >
+            <div className="flex flex-col gap-5" >
+                <div className="justify-start text-gray-900 text-9xl font-medium leading-10 ">
+                    Powerful Digital <br /> Branding Agency 
+                </div>
+                <div className=" text-gray-900 text-2xl font-normal leading-10">
+                    Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse <br /> donec. Vitae integer massa in blandit.
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default HeroSection;
