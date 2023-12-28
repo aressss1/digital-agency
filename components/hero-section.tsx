bb02bd83008b778ce316764cbc718cba2832a0a6
aressss1 <codewithfaizi@gmail.com>
2023-12-28 11:18:28 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
     return ( 
         <div className="flex flex-col lg:px-20 md:px-1 px-4 py-5 bg-stone-50 " >
             <div className="flex flex-col gap-5" >
-                <div className="justify-start text-gray-900 text-9xl font-medium leading-10 ">
+                <div className="justify-start text-gray-900 text-8xl font-medium lg:leading-[7.5rem] ">
                     Powerful Digital <br /> Branding Agency 
                 </div>
                 <div className=" text-gray-900 text-2xl font-normal leading-10">
