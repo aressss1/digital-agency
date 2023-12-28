9b150c3c8f8fd4c72f2d69ae66d950a695010e25
aressss1 <codewithfaizi@gmail.com>
2023-12-28 11:26:33 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,8 +5,14 @@ const HeroSection = () => {
                 <div className="justify-start text-gray-900 text-8xl font-medium lg:leading-[7.5rem] ">
                     Powerful Digital <br /> Branding Agency 
                 </div>
-                <div className="w-[55rem] text-gray-900 text-2xl font-normal leading-10">
+                <div className="w-[55rem] text-gray-900 text-2xl font-normal leading-9">
                     Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.
+                </div>
+            </div>
+
+            <div className="flex gap-2" >
+                <div className="bg-orange rounded-3xl w-96 h-52   " >
+
                 </div>
             </div>
         </div>
