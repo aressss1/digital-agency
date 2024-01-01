3a79b7e21837ad742c9126a4f9ea63c97ee1eac6
aressss1 <codewithfaizi@gmail.com>
2024-01-01 12:52:54 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
                 </div>
 
                 <div className="flex flex-col gap-4" >
-                    <div className="bg-orange flex justify-around items-center gap-4 md:rounded-[2.5rem] rounded-[1.5rem] md:w-[13rem] w-[10rem] md:h-[4rem] h-[3rem] p-3 " >
+                    <div className="bg-orange flex justify-around items-center gap-4 md:rounded-[2.5rem] rounded-[1.5rem] md:w-[13rem] w-[9rem] md:h-[4rem] h-[3rem] p-3 " >
                         <Image
                             src="Loading.png"
                             alt="loading"
@@ -71,7 +71,7 @@ const HeroSection = () => {
                             className="h-[40px]  md:h-[53px] lg:h-auto"
                         />
                     </div>
-                    <Button className="bg-amber text-black flex items-center justify-center gap-4 md:rounded-[2.5rem] rounded-[1.5rem] md:w-[13rem] w-[10rem] md:h-[4rem] h-[3rem]  " >
+                    <Button className="bg-amber text-black flex items-center justify-center gap-4 md:rounded-[2.5rem] rounded-[1.5rem] md:w-[13rem] w-[9rem] md:h-[4rem] h-[3rem]  " >
                         Know More
                         <ArrowRight className="h-5" />
                     </Button>
