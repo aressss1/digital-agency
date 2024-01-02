5dfcb17466b37f41304c4eaf92b4483913ac9155
aressss1 <codewithfaizi@gmail.com>
2024-01-02 16:09:44 +0500

    f-

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
     return (
         <div className=" bg-dark_blue" >
             <div className=" lg:px-20 md:px-14 px-4 lg:py-16 py-10 " >
-                <div className="bg-orange p-24 flex flex-col justify-between gap-5" >
+                <div className="bg-orange p-20 flex flex-col justify-between gap-9 rounded-[2rem] " >
 
                 <div className="flex justify-between md:flex-row flex-col md:gap-0 gap-1" >
-                    <div className="lg:w-[56rem] md:w-[40rem] text-stone-50 lg:text-7xl md:text-5xl text-3xl  font-medium lg:leading-[5rem] md:leading-[3.5rem] leading-[2.5rem] ">
-                        The projects we have done
+                    <div className="lg:w-[47rem] md:w-[40rem] text-stone-50 lg:text-7xl md:text-5xl text-3xl  font-medium lg:leading-[5rem] md:leading-[3.5rem] leading-[2.5rem] ">
+                        Have an Awesome Project?
                     </div>
                     <Button className="bg-orange text-white border border-stone-50 flex items-center gap-4 rounded-[2rem] lg:w-[13rem] md:w-[9rem]  lg:h-[5rem] md:h-[3rem]  justify-center md:self-center self-end " >
                         Know More
@@ -22,7 +22,7 @@ const Footer = () => {
                 <Separator />
 
                 <div className="flex flex-col  gap-4 self-start" >
-                    <div className="flex items-center" >
+                    <div className="flex items-center gap-4" >
                         <Image 
                             src="logo-1.svg"
                             alt="Logo"
@@ -32,7 +32,7 @@ const Footer = () => {
                             Branding Agency
                         </div>
                     </div>
-                    <div className=" text-stone-50 text-2xl font-medium ">
+                    <div className=" text-stone-50 text-2xl font-light w-[40rem]">
                         Eu scelerisque amet amet malesuada id ultrices morbi. Vitae purus mauris hac nisi habitasse donec. Vitae integer massa in blandit.
                     </div>
                 </div>
