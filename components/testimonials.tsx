3521aaf39ca38d2dba008d058d030a1d46ee0d84
aressss1 <codewithfaizi@gmail.com>
2024-01-04 10:47:41 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -7,30 +7,37 @@ const Testimonails = () => {
     const testimonialsContent = [
         {
             id: 1,
-            content: "‘I really enjoyed working with you guys, you guys are ver communicative and quick to do the job. I can’t wait to work with you again!!’",
-            authorName: "John Doe",
-            authorRole: "CEO at BigSpook",
+            content: "Working with Branding Agency's team was fantastic—great communication and swift execution. Looking forward to collaborating again soon!",
+            authorName: "Ryan Hoofman",
+            authorRole: "CEO at HighUp",
             authorImg: "/clients/client-1.jpg"
         },
-        // {
-        //     id: 2,
-        //     content: "‘I really enjoyed working with you guys, you guys are ver communicative and quick to do the job. I can’t wait to work with you again!!’",
-        //     authorName: "John Doe",
-        //     authorRole: "CEO at BigSpook",
-        //     authorImg: "/clients/client-1.png"
-        // },
-        // {
-        //     id: 3,
-        //     content: "‘I really enjoyed working with you guys, you guys are ver communicative and quick to do the job. I can’t wait to work with you again!!’",
-        //     authorName: "John Doe",
-        //     authorRole: "CEO at BigSpook",
-        //     authorImg: "/clients/client-1.png"
-        // },
+        {
+            id: 2,
+            content: "Branding Agency's efficiency and clear communication made working together a pleasure. Excited for the opportunity to collaborate again soon!",
+            authorName: "Austin Distel",
+            authorRole: "Founder at GrowTogether",
+            authorImg: "/clients/client-2.jpg"
+        },
+        {
+            id: 3,
+            content: "Collaborating with Branding Agency's team was smooth thanks to your their communication and efficiency. Looking forward to the next project together!",
+            authorName: "Mark Howard",
+            authorRole: "Founders at Builders",
+            authorImg: "/clients/client-3.jpg"
+        },
+        {
+            id: 4,
+            content: "I really appreciated your team's quick and effective communication. Can't wait to work together again!",
+            authorName: "Aatik Tasneem",
+            authorRole: "CEO at BigSpook",
+            authorImg: "/clients/client-4.jpg"
+        },
     ]
 
     return (
         <div className="bg-dark_blue" >
-            <div className="lg:px-20 md:px-14 px-4 lg:py-16 py-10" >
+            <div className="lg:px-24 md:px-14 px-4 lg:py-16 py-10" >
                 <Carousel>
                     <CarouselContent>
                         {testimonialsContent.map((testimonial) => (
@@ -47,7 +54,7 @@ const Testimonails = () => {
                                             <Image
                                                 src={testimonial.authorImg}
                                                 alt={testimonial.authorName}
-                                                className="h-28 w-28 rounded-full"
+                                                className="h-28 w-28 rounded-full object-cover "
                                             />
                                             <div className="flex flex-col gap-3 items-center justify-center" >
                                                 <div className=" text-stone-50 text-4xl font-medium" >
